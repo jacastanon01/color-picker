5c5e481bcf2eb24103ad379a900cd4c3695b553d
Jacob Castanon <jacastanon01@gmail.com>
2024-08-25 13:25:02 -0500

    added hotfix config

--- cmd/colorpicker/main.go
+++ cmd/colorpicker/main.go
@@ -1,12 +1,45 @@
 package main
 
-import (
-	"fmt"
+import rl "github.com/gen2brain/raylib-go/raylib"
 
-	"github.com/jacastanon01/color-picker/pkg/color"
-)
+type HSB struct {
+	Hue        float32 // 0 to 360
+	Saturation float32 // 0 to 1.0
+	Brightness float32 // 0 to 1.0
+}
 
-func main(){
-	colors := color.GenerateSpectrum()
-	fmt.Println(colors)
+func main() {
+	const w int32 = 900
+	const h int32 = 450
+	const min float32 = 0.5
+	const max float32 = 1.0
+
+	rl.InitWindow(w, h, "raylib [core] example - basic window")
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() && rl.IsWindowFocused() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+
+		spectrum := make([][]HSB, h)
+		for y := range spectrum {
+			spectrum[y] = make([]HSB, w)
+			for x := range spectrum[y] {
+				spectrum[y][x].Hue = float32(x) / float32(w) * 360
+				spectrum[y][x].Saturation = 1.0
+				percent := float32(y) / float32(h) // percentage of height
+		
+				spectrum[y][x].Brightness = (percent * (max - min)) + min
+
+				rl.DrawPixel(
+					int32(x), int32(y), rl.ColorFromHSV(spectrum[y][x].Hue, spectrum[y][x].Saturation, spectrum[y][x].Brightness),
+				)
+			}
+		}
+		rl.EndDrawing()
+	}
 }
